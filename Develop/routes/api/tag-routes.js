e2ec0696929197cba7c2e5c00d5b9e58e500fd9c
Michelle Wehr <michelle_wehr@icloud.com>
2021-10-13 13:06:01 -0400

    get routes for tags working

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -23,6 +23,27 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      through: ProductTag,
+      as: 'products'
+    }
+})
+.then(dbTagData => {
+  if(!dbTagData) {
+    res.status(404).json({ message: 'No tag with that id found'} );
+    return;
+  }
+  res.json(dbTagData);
+})
+.catch(err => {
+  console.log(err);
+  res.status(500).json(err);
+})
 });
 
 router.post('/', (req, res) => {
